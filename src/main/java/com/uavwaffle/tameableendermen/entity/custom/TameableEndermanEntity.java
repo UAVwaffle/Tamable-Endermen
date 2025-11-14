68a0fe923f94c710fcf1df6cc352af18e39ef259
UAVwaffle <68166007+UAVwaffle@users.noreply.github.com>
2025-11-13 19:43:35 -0500

    MINOR: MAJOR: - Setup Parchment Mappings

--- src/main/java/com/uavwaffle/tameableendermen/entity/custom/TameableEndermanEntity.java
+++ src/main/java/com/uavwaffle/tameableendermen/entity/custom/TameableEndermanEntity.java
@@ -5,7 +5,7 @@ import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.level.Level;
 
 public class TameableEndermanEntity extends EnderMan {
-    public TameableEndermanEntity(EntityType<? extends EnderMan> p_32485_, Level p_32486_) {
-        super(p_32485_, p_32486_);
+    public TameableEndermanEntity(EntityType<? extends EnderMan> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
     }
 }
